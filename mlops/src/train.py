11b2a865204214b17fcd1dfb6241cc97370021f4
Luiz Braz <luiz.braz@live.com>
2020-12-10 22:52:00 -0300

    Add train

---
+++ mlops/src/train.py
@@ -0,0 +1,42 @@
+# Databricks notebook source
+# MAGIC %run /Shared/churn-model/utils
+
+# COMMAND ----------
+
+seed = 2021
+target = 'Churn'
+
+drop_columns = [target, 'CodigoCliente']
+
+# Get the Train Dataset
+dataset = get_dataset('/dbfs/Dataset/Customer')
+
+# Preprocessing Features
+dataset, numeric_columns = preprocessing(dataset)
+
+# Split train and test
+train_dataset, test_dataset = split_dataset(dataset, seed)
+
+# Parameters we got from the best interaction
+params = {'early_stopping_rounds': 50, 
+          'learning_rate': 0.2258, 
+          'max_depth': 57, 
+          'maximize': False, 
+          'min_child_weight': 19.22, 
+          'num_boost_round': 1000, 
+          'reg_alpha': 0.01, 
+          'reg_lambda': 0.348, 
+          'verbose_eval': False,
+          'seed': seed}
+
+# Get X, y
+X_train, X_test, y_train, y_test = get_X_y(train_dataset, test_dataset, target, numeric_columns, drop_columns)
+
+# Train model and Load
+model_uri = train_model(X_train, y_train, X_test, y_test)
+model = load_model(model_uri)
+
+# Persist in the DBFS
+persist_model(model, '/dbfs/models/churn-prediction')
+
+print('Model trained')
