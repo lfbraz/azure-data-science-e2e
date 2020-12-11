1600a131c2ca457f94a04d7bec0b07c764850bec
Luiz Braz <luiz.braz@live.com>
2020-12-10 22:52:57 -0300

    Add deploy-aks

---
+++ mlops/src/deploy-aks.py
@@ -0,0 +1,30 @@
+# Databricks notebook source
+# MAGIC %run /Shared/churn-model/utils
+
+# COMMAND ----------
+
+# Workspace config
+workspace_name = 'e2e-datascience-aml'
+resource_group = 'RG-E2E-DATA-SCIENCE'
+subscription_id = 'f56912be-98e5-44e3-9e64-54bc52cef4a7'
+
+# Model
+model_name = 'churn-model'
+model_description = 'Model to predict churn'
+model_path = '/dbfs/models/churn-prediction'
+
+# Environment
+environment_name = 'xgboost_env'
+conda_dep_file = '/dbfs/models/churn-prediction/conda.yaml'
+entry_script = '/dbfs/models/churn-prediction/score.py'
+
+#Endpoint - PROD
+endpoint_name = 'api-churn-prod'
+aks_name = 'aks-e2e-1'
+
+workspace = get_workspace(workspace_name, resource_group, subscription_id)
+
+model_azure = register_model(workspace, model_name, model_description, model_path)
+
+inference_config = get_inference_config(environment_name, conda_dep_file, entry_script)
+service = deploy_aks(workspace, model_azure, endpoint_name, inference_config, aks_name)
