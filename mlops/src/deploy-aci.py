b4b63079f65d8f240a2c8d96eec00208416f63ad
Luiz Braz <luiz.braz@live.com>
2021-01-26 10:29:44 -0300

    Remove print

--- mlops/src/deploy-aci.py
+++ mlops/src/deploy-aci.py
@@ -20,7 +20,6 @@ entry_script = '/dbfs/models/churn-prediction/score.py'
 
 #Endpoint - DEV
 endpoint_name = 'api-churn-dev'
-print()
 
 workspace = get_workspace(workspace_name, resource_group, subscription_id)
 
