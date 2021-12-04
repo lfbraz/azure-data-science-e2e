d216808cbaffc40481e41c848efba7999fcdfe21
lfbraz <lfbraz>
2021-12-04 22:46:50 +0000

    Deploy Dev AKS

---
+++ mlops/src/deploy-aks-dev.py
@@ -0,0 +1,30 @@
+# Databricks notebook source
+# MAGIC %run ../src/utils
+
+# COMMAND ----------
+
+# Workspace config
+workspace_name = dbutils.secrets.get(scope = "azure-key-vault", key = "workspace-name")
+resource_group = dbutils.secrets.get(scope = "azure-key-vault", key = "resource-group")
+subscription_id = dbutils.secrets.get(scope = "azure-key-vault", key = "subscription-id")
+
+# Model
+model_name = 'churn-model'
+model_description = 'Model to predict churn'
+model_path = '/dbfs/models/churn-prediction'
+
+# Environment
+environment_name = 'xgboost_env'
+conda_dep_file = '/dbfs/models/churn-prediction/conda.yaml'
+entry_script = '/dbfs/models/churn-prediction/score.py'
+
+#Endpoint - PROD
+endpoint_name = 'api-churn-dev'
+aks_name = 'aks-e2e-1'
+
+workspace = get_workspace(workspace_name, resource_group, subscription_id)
+
+model_azure = get_model(workspace, model_name)
+
+inference_config = get_inference_config(environment_name, conda_dep_file, entry_script)
+service = deploy_aks(workspace, model_azure, endpoint_name, inference_config, aks_name)
