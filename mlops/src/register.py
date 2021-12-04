671b9387a7212c2f4b77c9c72d8c6a3f3d187158
lfbraz <lfbraz>
2021-12-04 23:46:38 +0000

    Change Model Name

--- mlops/src/register.py
+++ mlops/src/register.py
@@ -9,14 +9,10 @@ resource_group = dbutils.secrets.get(scope = "azure-key-vault", key = "resource-
 subscription_id = dbutils.secrets.get(scope = "azure-key-vault", key = "subscription-id")
 
 # Model
-model_name = 'churn-model'
+model_name = 'ChurnModel'
 model_description = 'Model to predict churn'
 model_path = '/dbfs/models/churn-prediction'
 
 workspace = get_workspace(workspace_name, resource_group, subscription_id)
 
 model_azure = register_model(workspace, model_name, model_description, model_path)
-
-# COMMAND ----------
-
-
