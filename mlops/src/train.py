c09ff1cab6df2d3eda89b7e55ebead3574d0a4af
lfbraz <lfbraz>
2022-03-23 12:15:49 +0000

    Alter learning_rate to 0.2262

--- mlops/src/train.py
+++ mlops/src/train.py
@@ -19,7 +19,7 @@ train_dataset, test_dataset = split_dataset(dataset, seed)
 
 # Parameters we got from the best interaction
 params = {'early_stopping_rounds': 50, 
-          'learning_rate': 0.2261, 
+          'learning_rate': 0.2262, 
           'max_depth': 65, 
           'maximize': False, 
           'min_child_weight': 19.22, 
